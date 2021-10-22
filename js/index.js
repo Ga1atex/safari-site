1a569abc614ec1409ba52a22471aee4b72b7ea71
Ga1atex <galatexxx@gmail.com>
2021-10-22 23:06:55 +0300

    added css variables

--- js/index.js
+++ js/index.js
@@ -27,4 +27,9 @@ $(function () {
       preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
     },
   });
+
+  $('.menu__btn').on('click', function (event) {
+    $('.menu__list').toggleClass('menu__list--active');
+    $('.menu__btn').toggleClass('menu__btn--active');
+  });
 });
